a766fa1258f87f9a4960b028386fbd3f8dce00dc
GaBaljaintheroom <junsu1222@naver.com>
2023-03-18 11:50:37 +0900

    dp-동적계획법

---
+++ 박준수 알고리즘/dp-동적계획법/1904번.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+
+for index in range(3, n + 1):
+    dp[index] = (dp[index - 1] + dp[index - 2]) % 15746
+
+
+print(dp[n])
+
+#https://www.acmicpc.net/problem/1904
