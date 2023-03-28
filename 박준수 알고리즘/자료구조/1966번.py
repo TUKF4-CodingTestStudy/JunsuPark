240746db3d2e3b1b2af29ace195e02a8a6d26e14
GaBaljaintheroom <junsu1222@naver.com>
2023-03-28 21:41:54 +0900

    자료구조 - 1966번

---
+++ 박준수 알고리즘/자료구조/1966번.py
@@ -0,0 +1,24 @@
+test_case = int(input())
+
+
+for i in range(test_case):
+    N, M = list(map(int, input().split()))
+    queue = list(map(int, input().split())) # 3 2 1
+    queue = [(i, idx) for idx, i in enumerate(queue)] # (3,0), (2,1), (1,0)
+
+
+    count = 0
+    while True:
+        if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+            count += 1
+            if queue[0][1] == M:
+                print(count)
+                break
+            else:
+                queue.pop(0)
+        else:
+            queue.append(queue.pop(0))
+
+# https://www.acmicpc.net/problem/1966
+
+
