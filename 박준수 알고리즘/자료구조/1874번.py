05b6d82a281c0ab5869d5c9d937b30e358cd9511
GaBaljaintheroom <junsu1222@naver.com>
2023-03-28 16:54:04 +0900

    자료구조 - 1874번

---
+++ 박준수 알고리즘/자료구조/1874번.py
@@ -0,0 +1,24 @@
+
+n = int(input())
+
+count = 1
+stack = []
+result = []
+
+for i in range(1, n + 1): # 데이터 개수만큼 반복
+    data = int(input())
+    while count <= data: # 입력 받은 데이터에 도달할 때까지 삽입
+        stack.append(count) # 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다고 하자
+        count += 1
+        result.append('+')
+
+    if stack[-1] == data: # 스택의 최상위 원소가 데이터와 같을 때 출력
+        stack.pop()         # pop 했을 때의 숫자 나열이 처음 입력했던 data 숫자 나열과 같아야 함
+        result.append('-')
+    else: # 불가능한 경우
+        print('\nNO')
+        exit(0)
+
+print('\n'.join(result)) # 가능한 경우
+
+# https://www.acmicpc.net/problem/1874
