1924aff8a6132c83efcf2f9b33bb42669c2bf8b2
GaBaljaintheroom <junsu1222@naver.com>
2023-03-31 16:56:46 +0900

    자료구조-10828번

---
+++ 박준수 알고리즘/자료구조/10828번.py
@@ -0,0 +1,50 @@
+import sys
+
+
+def push(x):
+    stack.append(x)
+
+
+def pop():
+    if len(stack) == 0:
+        print(-1)
+    else:
+        print(stack.pop())
+
+
+def size():
+    print(len(stack))
+
+
+def empty():
+    if len(stack) == 0:
+        print(1)
+    else:
+        print(0)
+
+
+def top():
+    if len(stack) == 0:
+        print(-1)
+    else:
+        print(stack[-1])
+
+input = sys.stdin.readline
+N = int(input())
+
+stack = []
+
+for _ in range(N):
+    cmd = list(input().split())
+    if cmd[0] == "push":
+        push(cmd[1])
+    elif cmd[0] == "pop":
+        pop()
+    elif cmd[0] == "size":
+        size()
+    elif cmd[0] == "empty":
+        empty()
+    elif cmd[0] == "top":
+        top()
+
+# https://www.acmicpc.net/problem/10828
