1dcbf80ace901228d03c9cf15c498e80993e1e77
GaBaljaintheroom <junsu1222@naver.com>
2023-06-26 11:37:20 +0900

    알고리즘 - 1697번

--- 박준수 알고리즘/알고리즘/탐색/1697번.py
+++ 박준수 알고리즘/알고리즘/탐색/1697번.py
@@ -12,7 +12,7 @@ def bfs():
         if now_pos == k:           # 같은 위치에 도달하게 되면
             return array[now_pos]  # 위치로 이동한 연산의 횟수를 반환
         for next_pos in (now_pos - 1, now_pos + 1, now_pos * 2):
-            if 0 <= next_pos <= MAX and not array[next_pos]:  # 범위 내에 있고 방문하지 않았다면
+            if 0 <= next_pos < MAX and not array[next_pos]:  # 범위 내에 있고 방문하지 않았다면
                 array[next_pos] = array[now_pos] + 1          # 각 위치에 지금껏 연산된 횟수를 저장
                 q.append(next_pos)
 
