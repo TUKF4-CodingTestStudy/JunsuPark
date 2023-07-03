6015d5eba1903cf52d70fffaeb42fd1ba0eefe46
GaBaljaintheroom <junsu1222@naver.com>
2023-07-04 00:56:46 +0900

    알고리즘 - 2012번

---
+++ 박준수 알고리즘/알고리즘/그리디/2012번.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+array = []
+
+for _ in range(n):
+    array.append(int(input()))
+
+# 오름차순 정렬
+array.sort()
+
+# 불만도의 합 계산
+result = 0
+for i in range(1, len(array) + 1):
+    result += abs(i - array[i - 1])
+
+print(result)
